6a92fed85197e0ff303aa459d2f3201697844760
8hourssleep <phupha.a@kkumail.com>
2025-11-30 16:35:46 +0700

    tools route

--- lib/features/api/apiSlice.ts
+++ lib/features/api/apiSlice.ts
@@ -1,26 +1,81 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import type { Tool } from "../tools/toolSlice";
-export type { Tool };
+// รอinterface ดีๆ
+export type ToolCategory =
+  | "men's clothing"
+  | "jewelery"
+  | "electronics"
+  | "women's clothing";
+export type ToolStatus = "available" | "unavailable";
+export interface Tool {
+  id: number;
+  url: string;
+  title: string;
+  description: string;
+  available: number;
+  quatity: number;
+  assetId: string | undefined;
+  category: ToolCategory;
+}
+export interface Transaction {
+  id: string;
+  user_id: string;
+  create_at: string;
+  approve_at: string;
+  loan_at: string;
+  recieve_at: string;
+  status: ToolStatus;
+  message: string[]
+  update_at: string
+}
 
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "https://fakestoreapi.com" }),
-  tagTypes: ["Tool"],
+  tagTypes: ["Tool", "Transaction"],
   endpoints: (builder) => ({
     getTools: builder.query<Tool[], void>({
-    //   query: () => ({ url: "/products", method: "GET" }),
-      query: () => '/products',
-      providesTags: ["Tool"],
+      query: () => "/products",
+      providesTags: (result = [], error, arg) => [
+        "Tool",
+        ...result.map(({ id }) => ({ type: "Tool", id } as const)),
+      ],
     }),
     getTool: builder.query<Tool, string>({
       query: (toolId) => `/products/${toolId}`,
+      providesTags: (result, error, arg) => [{ type: "Tool", id: arg }],
     }),
-    addTool: builder.mutation<Tool[], Tool>({
-      query: (tool) => ({ url: "/products", method: "PUT", body: tool }),
+    addTool: builder.mutation<Tool, Tool>({
+      query: (tool) => ({ url: "/products", method: "POST", body: tool }),
       invalidatesTags: ["Tool"],
     }),
+    editTool: builder.mutation<Tool[], Tool>({
+      query: (tool) => ({ url: "/products", method: "PATCH", body: tool }),
+      invalidatesTags: ["Tool"],
+    }),
+    deleteTool: builder.mutation<Tool, string>({
+      query: (toolId) => ({ url: "/products", method: "DELETE", body: toolId }),
+      invalidatesTags: ["Tool"],
+    }),
+
+    // getTransactions: builder.query<Transaction[], void>({
+    //     query: () => "/transactions",
+    //     providesTags: (result = [], error, arg) => [
+    //     "Tool",
+    //     ...result.map(({ id }) => ({ type: "Transaction", id } as const)),
+    //   ],
+    // }),
+    // getTransaction: builder.query<Transaction, string>({
+    //   query: (transactionId) => `/transactions/${transactionId}`,
+    //   providesTags: (result, error, arg) => [{ type: "Transaction", id: arg }],
+    // }),
   }),
 });
 
-export const { useGetToolsQuery, useGetToolQuery } = apiSlice;
-// ใช้fieldต่างๆกับ ui เหอะ
+export const {
+  useGetToolsQuery,
+  useGetToolQuery,
+  useEditToolMutation,
+  useAddToolMutation,
+  useDeleteToolMutation,
+} = apiSlice;
+// keepUnusedDataFor  -> for set catch time , default 60s
--- lib/store.ts
+++ lib/store.ts
@@ -1,14 +1,15 @@
 "use client";
 import type { Action, ThunkAction } from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit";
-
+import { apiSlice } from "./features/api/apiSlice";
 const store = configureStore({
   reducer: {
-    // state in field name: slice import name
+    [apiSlice.reducerPath]: apiSlice.reducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
 });
 export default store;
-// types
+
 export type AppStore = typeof store;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
