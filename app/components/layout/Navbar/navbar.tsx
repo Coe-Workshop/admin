67f067a86a7888190d5a15ca974fcd2f1d315ce3
kitsada babthong <kritsada.ba@kkumail.com>
2025-11-30 22:27:36 +0700

    CW-21 navslide

--- app/components/layout/Navbar/navbar.tsx
+++ app/components/layout/Navbar/navbar.tsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 import Link from "next/link";
 import useDisclosure from "@/app/hook/useDisclosure";
 import Modal from "@/app/components/ui/modal/modal";
-import NavSlide from "@/app/components/layout/Navbar/navslide";
-import {useModal} from "@/app/hook/useModal";
+import NavSlide from "@/app/components/layout/navbar/navslide";
+import { useModal } from "@/app/hook/useModal";
 import ModalContainer from "@/app/components/layout/NavbarContainer/modalContainer";
 
 function Navbar() {
   const { opened, handle } = useDisclosure();
-  const { modalState, handle:handleModal } = useModal();
+  const { modalState, handle: handleModal } = useModal();
   const menuMapProps: MenuMapProps[] = [
     { title: "Quick Create", path: "/create" },
     { title: "Transaction", path: "/transaction" },
@@ -59,77 +59,80 @@ function Navbar() {
   });
 
   return (
-      <>
-    <div className={styles.navbar}>
-      <div className={styles.navbar_inner}>
-        <div>
-          <div className={styles.logo}>
-            <p className={styles.logo_mark}>EN</p>
-            <p className={styles.logo_dot}>.W</p>
-            <p className={styles.logo_status}>Admin</p>
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.navbar_inner}>
+          <div>
+            <div className={styles.logo}>
+              <p className={styles.logo_mark}>EN</p>
+              <p className={styles.logo_dot}>.W</p>
+              <p className={styles.logo_status}>Admin</p>
+            </div>
+          </div>
+          <div className={styles.box}>
+            <button
+              className={styles.button_create}
+              onClick={() => handleModal.open("create")}
+            >
+              <Image
+                className={styles.action_plus}
+                width={22}
+                height={22}
+                alt="plus_icon"
+                src={"/Navbar/plus.svg"}
+              ></Image>
+              <p className={styles.text}>Quick Create</p>
+            </button>
+            <Image
+              onClick={() => handle.open()}
+              className={styles.action_hamberger}
+              width={120}
+              height={120}
+              alt="hamberger_icon"
+              src={"/Navbar/hamberger.svg"}
+            ></Image>
+            {menuBlog}
           </div>
         </div>
-        <div className={styles.box}>
-          <button className={styles.button_create} onClick={()=> handleModal.open("create")}>
+        <div className={styles.tab_admin}>
+          <div className={styles.admin}>
             <Image
-              className={styles.action_plus}
-              width={22}
-              height={22}
-              alt="plus_icon"
-              src={"/Navbar/plus.svg"}
+              className={styles.blog_icon}
+              src={Admin.profile}
+              width={40}
+              height={40}
+              alt={Admin.title}
             ></Image>
-            <p className={styles.text}>Quick Create</p>
-          </button>
-          <Image
-            onClick={() => handle.open()}
-            className={styles.action_hamberger}
-            width={120}
-            height={120}
-            alt="hamberger_icon"
-            src={"/Navbar/hamberger.svg"}
-          ></Image>
-          {menuBlog}
-        </div>
-      </div>
-      <div className={styles.tab_admin}>
-        <div className={styles.admin}>
+            <div>
+              <p className={styles.name}>{Admin.name}</p>
+              <p className={styles.email}>{Admin.email}</p>
+            </div>
+          </div>
           <Image
             className={styles.blog_icon}
-            src={Admin.profile}
-            width={40}
-            height={40}
+            src={Admin.icon}
+            width={20}
+            height={20}
             alt={Admin.title}
           ></Image>
-          <div>
-            <p className={styles.name}>{Admin.name}</p>
-            <p className={styles.email}>{Admin.email}</p>
-          </div>
         </div>
-        <Image
-          className={styles.blog_icon}
-          src={Admin.icon}
-          width={20}
-          height={20}
-          alt={Admin.title}
-        ></Image>
+        <ModalContainer margin="0" opened={opened} onClose={handle.close}>
+          <NavSlide
+            menuMapPropsList={menuMapProps}
+            onClose={handle.close}
+          ></NavSlide>
+        </ModalContainer>
       </div>
-      <ModalContainer margin="0" opened={opened} onClose={handle.close}>
-        <NavSlide
-          menuMapPropsList={menuMapProps}
-          onClose={handle.close}
-        ></NavSlide>
-      </ModalContainer>
-    </div>
-  <div
-      onClick={() => console.log("click")}
-      style={{ position: "absolute" }}
-  >
-    <Modal
-        modalState={modalState}
-        onClose={() => handleModal.close("create")}
-    ></Modal>
-  </div>
-  </>
+      <div
+        onClick={() => console.log("click")}
+        style={{ position: "absolute" }}
+      >
+        <Modal
+          modalState={modalState}
+          onClose={() => handleModal.close("create")}
+        ></Modal>
+      </div>
+    </>
   );
 }
 
