df326931d53714f80c5705c30e7deee44ab90ee6
kitsada babthong <kritsada.ba@kkumail.com>
2025-12-08 22:49:16 +0700

    CW-39 setup

--- app/(main)/onboarding/page.tsx
+++ app/(main)/onboarding/page.tsx
@@ -1,9 +1,21 @@
+"use client"
+
 import { TextInput } from "@/app/components/ui/textInput/textInput";
 import styles from "./onboarding.module.scss";
+import { useState } from "react";
 const OnBoarding = () => {
+  //เขียน handleSubmitตรงนี้ อย่าลืม e.preventDefault()
   return (
     <div>
-      <TextInput title="ชื่อ" placeholder="กรอกชื่อของคุณ" require></TextInput>
+      <form>
+        {/* เรียก onsubmit handler บน onsubmit ใน tag form*/}
+        <TextInput
+          title="ชื่อ"
+          placeholder="กรอกชื่อของคุณ"
+          require
+        ></TextInput>
+        <button type="submit"></button>
+      </form>
     </div>
   );
 };
