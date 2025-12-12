6b50258d23524965626bcb279065ee3dda1c6edc
kitsada babthong <kritsada.ba@kkumail.com>
2025-12-12 21:32:17 +0700

    CW-61 Table admin

--- app/(main)/tools/test/page.tsx
+++ app/(main)/tools/test/page.tsx
@@ -5,18 +5,44 @@ import { ModalContainer } from "@/app/components/modal/modalContainer/modalConta
 import { ItemTransaction } from "@/app/components/ui/itemTransaction/itemTransaction";
 import useDisclosure from "@/app/hook/useDisclosure";
 import { DeleteConfirm } from "@/app/components/modal/deleteConfirm/deleteConfirm";
+import styles from "./test.module.scss";
+import Image from "next/image";
+import { prefix } from "@/app/utils/prefix";
 const Tool = () => {
   const [itemanme] = useState("itemName");
   const [category] = useState("category");
+  const [description] = useState(
+    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
+  );
   const { opened, handle } = useDisclosure();
   return (
     <div>
-      <section>
-        <div>
-          <h1>{itemanme}</h1>
-          <p>{category}</p>
+      <section className={styles.info}>
+        <div className={styles.header}>
+          <div className={styles.title}>
+            <h1>{itemanme}</h1>
+            <p className={styles.category}>{category}</p>
+          </div>
+          <div className={styles.action}>
+            <button type="button">
+              <Image
+                src={`${prefix}/icon/pencil.svg`}
+                width={24}
+                height={24}
+                alt="editIcon"
+              ></Image>
+            </button>
+            <button type="button">
+              <Image
+                src={`${prefix}/icon/tash.svg`}
+                width={24}
+                height={24}
+                alt="editIcon"
+              ></Image>
+            </button>
+          </div>
         </div>
-        <div></div>
+        <p className={styles.description}>{description}</p>
       </section>
       <section>
         <ItemTransaction></ItemTransaction>
--- app/components/modal/deleteConfirm/deleteConfirm.tsx
+++ app/components/modal/deleteConfirm/deleteConfirm.tsx
@@ -7,20 +7,28 @@ export const DeleteConfirm = ({
 }: DeleteConfirmProps) => {
   return (
     <div className={styles.deleteConfirm}>
-      <h2>ยืนยันการลบเลขครุภัณฑ์</h2>
-      <p>
-        เลขครุภัณฑ์จะถูกลบไปอย่างถาวร ประวัติหรือข้อมูลอื่นๆ
-        ที่เกี่ยวข้องจะไม่สามารถย้อนกลับได้อีก กรุณาพิม{" "}
-        <span>{confirmMessage + " "}</span>
-        เพื่อยืนยันการลบ
-      </p>
-      <TextInput></TextInput>
-      <div className={styles.action}>
-        <button className={styles.cancel} type="button">
-          ยกเลิก
-        </button>
-        <button className={styles.confirm}>ลบรายการ</button>
+      <div className={styles.header}>
+        <h2>ยืนยันการลบเลขครุภัณฑ์</h2>
+        <p>
+          เลขครุภัณฑ์จะถูกลบไปอย่างถาวร ประวัติหรือข้อมูลอื่นๆ
+          ที่เกี่ยวข้องจะไม่สามารถย้อนกลับได้อีก กรุณาพิม{" "}
+          <span>{confirmMessage + " "}</span>
+          เพื่อยืนยันการลบ
+        </p>
       </div>
+      <form action="">
+        <TextInput></TextInput>
+        <div className={styles.action}>
+          <button
+            className={styles.cancel}
+            onClick={() => onClose()}
+            type="button"
+          >
+            ยกเลิก
+          </button>
+          <button className={styles.confirm}>ลบรายการ</button>
+        </div>
+      </form>
     </div>
   );
 };
