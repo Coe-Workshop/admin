ccc956aec2d09abf71dd5931efd434b13d25ff28
8hourssleep <phupha.a@kkumail.com>
2026-02-13 22:12:19 +0700

    attatch credential wit redux request in include way

--- lib/features/apiSlice.ts
+++ lib/features/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const BASE_URL = "https://dev-coe.ionize13.com/api";
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL, credentials:"include" }),
   tagTypes: ["Tools", "Transaction"],
   endpoints: () => ({}),
 });
