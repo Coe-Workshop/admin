041b104b431403fd5cec1725a2acd517ad0127d3
kitsada babthong <kritsada.ba@kkumail.com>
2026-02-23 21:59:27 +0700

    wapper transaction

--- app/(main)/transaction/page.tsx
+++ app/(main)/transaction/page.tsx
@@ -1,73 +1,10 @@
-"use client";
-import { Select } from "@/app/components/form/Select/Select";
-import DatePicker from "@/app/components/ui/Datepicker/Datepicker";
-import { AdminTransaction } from "@/app/components/ui/adminTransaction/adminTransaction";
-import { Status } from "@/app/types/api/transaction";
-import { useState } from "react";
-import styles from "./transaction.module.scss";
-import { ResponseStatus } from "@/app/components/ui/adminTransaction/adminTransaction.type";
-import { useSetQuery } from "@/app/hook/SearchQuery";
-import { toISODateStringOrNull } from "@/app/utils/ISODateStringHandle";
-import { TextInput } from "@/app/components/form/TextInput/TextInput";
-
-const Transaction = () => {
-  const setQuery = useSetQuery();
-
-  const [itemId, setItemId] = useState<string>("");
-  const [responseStatus, setResponseStatus] = useState<ResponseStatus>(
-    ResponseStatus.Approve,
-  );
-  const [message, setMessage] = useState<string>("");
-  const hadleStutusChange = () => {
-    console.log("submit");
-  };
-  // const [dateFilter, setDateFilter] = useState<ISODateString | undefined>(undefined);
-  const [statusFilter, setStatusFilter] = useState<Status | undefined>(undefined);
-
-  const handleDateChange = (newDate: Date | null | undefined) => {
-    // setDateFilter(toISODateStringOrUndefined(newDate));
-    // idk why it เลื่อนไปข้างหลังวันนึง
-    newDate?.setDate(newDate.getDate()+1);
-    setQuery("date", toISODateStringOrNull(newDate));
-    newDate?.setDate(newDate.getDate()-1);
-  }
+import { Suspense } from "react";
+import { Transaction } from "./transactionClient";
+const PageTransaction = () => {
   return (
-    <div>
-      <div className={styles.filter}>
-        <TextInput
-          label="ไอดีของอุปกรณ์"
-          placeholder="กด 1 เพื่อขึ้นเรือ, 47 เพื่อเทส"
-          require
-          value={itemId}
-          onChange={(newVar) => {
-            setItemId(newVar);
-            setQuery("item", newVar);
-          }}
-        ></TextInput>
-        <DatePicker 
-          placeholder="--/--/----" 
-          required={true}
-          onChange={handleDateChange}>
-        </DatePicker>
-        <Select
-          placeholder="ตัวกรองสถานะ"
-          onChange={(newValue) => {
-            setStatusFilter(newValue as Status | undefined);
-            setQuery("status", newValue as Status | null);
-          }}
-          value={statusFilter}
-          options={Object.keys(Status)}
-        ></Select>
-      </div>
-
-      <AdminTransaction
-        message={message}
-        onChange={setMessage}
-        onSubmit={hadleStutusChange}
-        responseStatus={responseStatus}
-        setResponseStatus={setResponseStatus}
-      ></AdminTransaction>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Transaction />
+    </Suspense>
   );
 };
-export default Transaction;
+export default PageTransaction;
