2c464d91bcaf42b884c014b73bc604c34c13c937
VVeb1250 <teethawat.k@kkumail.com>
2026-02-23 22:58:15 +0900

    fix: proxy can disable

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,15 @@
 import type { NextRequest } from "next/server";
 import { checkAuthSession } from "@/lib/proxy/authProxy"; 
 
+function shouldEnableProxy() {
+  return process.env.PROXY_DISABLE?.toLowerCase() !== "true";
+}
+
 export function proxy(request: NextRequest) {
   // console.log("Enter URL:", request.nextUrl.pathname);
-  return checkAuthSession(request);
+  if (shouldEnableProxy()) {
+    return checkAuthSession(request);
+  }
 }
 
 export const config = {
